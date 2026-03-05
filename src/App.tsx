8ab932ae76faf88df6db67d006549660fa89d212
Mantra IT Team <it-team@mantracare.com>
2026-03-05 12:57:08 +0530

    Configure subpath hosting and Docker CI deployment

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vibe_tracker">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
